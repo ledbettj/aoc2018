f519807da65c7292b4fa9724d56210d301ed08ce
John Ledbetter <john.ledbetter@gmail.com>
2018-12-01 12:44:19 -0500

    add day1.part2

--- src/day1/core.clj
+++ src/day1/core.clj
@@ -9,6 +9,23 @@
   (with-open [rdr (io/reader file-name)]
     (doall (map parse-int (line-seq rdr)))))
 
-
-(defn run [initial-value inputs]
+; day1 part 1
+(defn run1 [initial-value inputs]
   (reduce + initial-value inputs))
+
+(defn run2-iter
+  [state]
+  (let [{inputs :inputs} state]
+    (reduce (fn [history value]
+              (let [{ seen :seen sum :sum} history
+                    new (+ sum value)]
+                (if (contains? seen new)
+                  (reduced new)
+                  { :seen (conj seen new) :sum new :inputs inputs })))
+            state inputs)))
+
+
+(defn run2
+  [initial-value inputs]
+  (first (filter #(instance? Long %)
+              (iterate run2-iter {:inputs inputs :seen (hash-set initial-value) :sum initial-value }))))
--- test/day1/core_test.clj
+++ test/day1/core_test.clj
@@ -5,5 +5,11 @@
 (deftest day1-part1
   (testing "Day 1 Part 1"
     (let [ [input] [(load-input "inputs/day1.txt")]
-          [results] [(run 0 input)]]
-      (is (= results 408)))))
+          [results] [(run1 0 input)]]
+      (is (= 408 results)))))
+
+(deftest day1-part2
+  (testing "Day 1 Part 2"
+    (let [ [input] [(load-input "inputs/day1.txt")]
+          [results] [(run2 0 input)]]
+      (is (= 55250 results)))))
