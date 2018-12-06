2bb57751c9b535ca80471c05a49544082ac7259f
John Ledbetter <john@callrail.com>
2018-12-06 09:06:05 -0500

    omg i hate you day4

--- src/aoc2018/day4.clj
+++ src/aoc2018/day4.clj
@@ -24,44 +24,86 @@
        (let [id (if (contains? e :id) (:id e) (:id (last results)))]
          (conj results (conj e {:id id })))) [] sorted-list)))
 
+(defn ensure-entry
+  "Ensure that there is an en entry in the state map for the given guard id"
+  [state guard-id]
+  (if (contains? state guard-id)
+    state
+    (conj state { guard-id { :events [] :sleep-map {} :asleep-for 0 } })))
 
-(defn new-guard-state [first-event]
-  { :state (:event first-event) :asleep-for 0 :list (vector first-event) })
 
-(defn update-guard-state [guard-state event]
-  (println "update-guard-state on " guard-state "with" event)
-  (let [new-state (:event event) current-state (:state guard-state) last-event (last (:list guard-state))]
-    (update
-     (let [updated-state (conj guard-state { :state new-state })]
-       (println "moving from " current-state "to" new-state)
-       (if (and (= new-state :awake) (= current-state :asleep))
-         (update updated-state :asleep-for + (t/in-minutes (t/interval (:time last-event) (:time event))))
-         updated-state)
-       ) :list conj event)))
+(defn ensure-min-in-sleep-map
+  [sleep-map minute]
+  (if (contains? sleep-map minute)
+    sleep-map
+    (conj sleep-map { minute 0 })))
 
-(defn process-guard-event-list
+(defn update-sleep-map
+  [sleep-map day minute]
+
+  (update
+   (ensure-min-in-sleep-map sleep-map minute)
+   minute
+   inc))
+
+(defn mark-asleep
+  [data day minute]
+  (let [data (update data :asleep-for inc)]
+    (update data :sleep-map update-sleep-map day minute)))
+
+(defn update-guard-data
+  [data event]
+  (let [last-event (last (:events data))
+        new-data (update data :events conj event)]
+    (if (and last-event (= (:event last-event) :asleep) (= (:event event) :awake))
+      ;; need to record time from (:time last-event) to (:time event) as asleep
+      (let [start-time (:time last-event) duration (t/in-minutes (t/interval start-time (:time event)))]
+        (reduce (fn [data n]
+                  (let [when (t/plus start-time (t/minutes n))
+                        day (t/day when)
+                        min (t/minute when)]
+                    (mark-asleep data day min))
+                  ) new-data (range 0 duration)))
+      ;; otherwise nothing
+      new-data)))
+
+(defn process-event-for-guard
+  "Update the state map with the given event for the given guard"
+  [state guard-id prev-event event]
+  (let [new-state (update state guard-id update-guard-data event)]
+    (if (not= guard-id (:id prev-event))
+      (update new-state (:id prev-event) update-guard-data event)
+      new-state)))
+
+
+(defn process-event
+  [state [prev-event event]]
+  (let [id (:id event)
+        prev-id (:id prev-event)
+        state (ensure-entry state (:id event))]
+    (process-event-for-guard state id prev-event event)))
+
+(defn process-event-list
   [event-list]
-  (let [first-event (first event-list)
-        initial-state { (:id first-event) (new-guard-state first-event) }]
-    (reduce (fn [state [prev-event event]]
-              (if (not= (:id event) (:id prev-event))
-                ;;  mark last guard as awake
-                (update 
-                 ;; start new guard 
-                 (conj state { (:id event) (new-guard-state event) })
-                 (:id prev-event) update-guard-state event)
-                ;; else
-                (update state (:id event) update-guard-state event)))
-               initial-state (partition 2 1 event-list))))
+  (let [first-event (first event-list)]
+    (reduce process-event
+            { (:id first-event) { :events [first-event] :sleep-map {} :asleep-for 0 } }
+            (partition 2 1 event-list))))
 
 
-;; day4-input
-;; (process-guard-event-list (day4-input))
-;;
-(defn try-to-solve-this-madness []
-  (let [sleepy-guard-state (last (sort-by (fn [item] (:asleep-for item)) (vals (process-guard-event-list (day4-input)))))
-        duration (:asleep-for sleepy-guard-state)]
-    sleepy-guard-state
+(defn solve-this-garbage
+  []
+  (let [results (vals (process-event-list (day4-input)))
+        sleepiest (last (sort-by (fn [entry] (:asleep-for entry)) results))
+        id (:id (first (:events sleepiest)))
+        sleep-map (:sleep-map sleepiest)
+        ]
+    id
+    ;; sleep-map
     ))
 
-(try-to-solve-this-madness)
+
+;; (solve-this-garbage)
+;; most asleep is at m40
+;; id is 2351
+;; (* 2351 40)
