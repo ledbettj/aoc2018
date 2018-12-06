6e731fde3f946ed467c6ecda130e6e08f6d7e470
John Ledbetter <john@callrail.com>
2018-12-06 10:01:40 -0500

    day5

--- src/aoc2018/day5.clj
+++ src/aoc2018/day5.clj
@@ -3,7 +3,7 @@
   (:require [clojure.string :as str]))
 
 
-(defn day5-input [] (load-input 5))
+(defn day5-input [] (first (load-input 5)))
 
 (defn reduce-polymer
   [polymer]
@@ -27,6 +27,27 @@
       (fully-reduce-polymer reduced))))
 
 
+(defn remove-polymer-unit
+  [polymer unit]
+  (str/replace polymer
+               (re-pattern (str "[" (str/lower-case unit) (str/upper-case unit) "]"))
+               ""))
 
+(defn a-to-z
+  []
+  (map char (range (int \a) (inc (int \z)))))
+
+(defn solve-part2
+  []
+  (let [input (day5-input)
+        results (map (fn [ch]
+                       (let [cnt (count (fully-reduce-polymer (remove-polymer-unit input ch)))]
+                         (println ch "reduced to" cnt)
+                         cnt))
+                     (a-to-z))]
+    (reduce min results)))
+
+;; p1:
 ;; (count (fully-reduce-polymer (day5-input)))
-
+;; p2:
+;; (solve-part2)
