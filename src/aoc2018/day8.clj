e55b404f1eb464b67ee48f3e3271ba35b8e688fd
John Ledbetter <john.ledbetter@gmail.com>
2018-12-08 09:29:24 -0500

    day8 p2

--- src/aoc2018/day8.clj
+++ src/aoc2018/day8.clj
@@ -35,8 +35,38 @@
    (reduce + 0 (:metadata node))
    (apply + (map sum-metadata (:children node)))))
 
+;; this does no fancy memoization to remember already calculated values :(
+(defn fancy-checksum
+  "Checksum logic for p2"
+  [node]
+  (let [children (:children node)
+        metadata (:metadata node)]
+    (if (empty? children)
+      (reduce + 0 metadata)
+      (reduce + 0
+              (map
+               (fn [index]
+                 (let [offset (- index 1)]
+                   (if (and (not= -1 offset) (contains? children offset))
+                   (fancy-checksum (nth children offset))
+                   0 ;; no child with index, val is 0
+                   ))) metadata))
+      )))
 
-;; test data
+
+;; p1 test data
 ;; (sum-metadata (first (read-node '(2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2))))
 ;; p1 answer
 ;; (let [tree (first (read-node (day8-input))) sum (sum-metadata tree)] sum)
+
+
+;; p2 test
+;; (let [tree (first (read-node '(2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2)))
+;;       sum (fancy-checksum tree)]
+;;   sum)
+;; p2 solution
+;; (let [tree (first (read-node (day8-input)))
+;;      sum (fancy-checksum tree)]
+;;  sum)
+
+
