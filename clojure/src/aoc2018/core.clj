05bfc34927bad04d5017b4786c505638d27d7973
John Ledbetter <john@callrail.com>
2018-12-11 09:14:15 -0500

    fixup input path

--- clojure/src/aoc2018/core.clj
+++ clojure/src/aoc2018/core.clj
@@ -9,7 +9,7 @@
 (defn load-input
   "Read the input file for the given day [1-25] and return an array of lines"
   [day]
-  (with-open [rdr (io/reader (str "inputs/day" day ".txt"))]
+  (with-open [rdr (io/reader (str "../inputs/day" day ".txt"))]
     ;; must use doall to force invocation; `map` is lazy and if we dont
     ;; force invocation it will be evaluated later, after the reader is closed.
     (doall (line-seq rdr))))
